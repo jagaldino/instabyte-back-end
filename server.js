31baef3e43019dde62485e509132e0c32ea988ac
jagaldino <arthurgaldino811@gmail.com>
2024-11-21 21:04:41 -0300

    :recycle: Aula 03 - Conectando sua API ao MongoDB: Estrutura, Conexão e Refatoração

--- server.js
+++ server.js
@@ -1,55 +1,10 @@
 import express from "express";
-
-const posts = [
-  {
-      id: "1",
-      descricao: "Uma foto teste",
-      imagem: "https://placecats.com/millie/300/150"
-  },
-  {
-      id: "2",
-      descricao: "Gato fofo dormindo",
-      imagem: "https://placekitten.com/400/200"
-  },
-  {
-      id: "3",
-      descricao: "Paisagem montanhosa",
-      imagem: "https://picsum.photos/seed/picsum/600/400"
-  },
-  {
-      id: "4",
-      descricao: "Cachorro brincando",
-      imagem: "https://placeimg.com/640/480/animals"
-  },
-  {
-      id: "5",
-      descricao: "Comida deliciosa",
-      imagem: "https://loremflickr.com/640/480/food"
-  },
-  {
-      id: "6",
-      descricao: "Cidade à noite",
-      imagem: "https://source.unsplash.com/random/600x400/?city,night"
-  }
-];
+import routes from "./src/routes/postsRoutes.js";
 const app = express();
-app.use(express.json())
 
+routes(app);
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
-  console.log("Servidor escutando!");
+  console.log("Servidor escutando na porta 3000!");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
-
-function buscarPostPorId(id) {
-  return posts.findIndex((post) =>{
-    return post.id == Number(id)
-  })
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostPorId(req.params.id)
-  res.status(200).json(posts[index]);
-});
